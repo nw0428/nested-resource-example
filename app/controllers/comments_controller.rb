94ebb0dcd807b282c5c307fc282597a809db1cf1
David Zabner <david@cazena.com>
2017-11-01 23:27:41 -0400

    Added delete comment link

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     redirect_to @painting
   end
 
+  def destroy
+    @painting = Painting.find(params[:painting_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @painting, notice: 'Comment was deleted'
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:content)
